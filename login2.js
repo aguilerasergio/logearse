1fde9b42ce9c4786c0c5894afcf5910dba6460f3
aguilerasergio <sergioaguilera1821@gmail.com>
2023-07-31 21:42:38 -0300

    primeros archivos

---
+++ login2.js
@@ -0,0 +1,29 @@
+
+function logear()
+{
+let user=document.getElementById("usuario").value;
+let pass=document.getElementById("clave").value;
+
+if(user=="sergio" && pass=="1234")
+{
+
+    window.location="bienvenido.html";
+
+} 
+
+ else
+    {
+
+        alert("Datos incorrectos");
+
+    }
+
+
+
+
+ 
+
+
+
+
+}
